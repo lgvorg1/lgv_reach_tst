12b1bfb58c34d2692a6d77493cdf35a8596c98e3
LuisGarcia <luis.garcia@xygeni.io>
2024-09-20 09:06:49 +0200

    first commit

---
+++ src/main/java/com/endor/MainClass.java
@@ -0,0 +1,16 @@
+
+package com.endor;
+
+
+import org.springframework.beans.CachedIntrospectionResults;
+import org.springframework.beans.BeanUtils;
+
+public class MainClass {
+
+  public static void main(String[] args) {
+    CachedIntrospectionResults.acceptClassLoader(MainClass.class.getClassLoader());
+    BeanUtils.getPropertyDescriptors(MainClass.class);
+  }
+
+}
+
